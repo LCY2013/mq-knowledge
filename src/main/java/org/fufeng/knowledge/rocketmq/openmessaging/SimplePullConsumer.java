47a9d839593b9d97bd87f0450ddec082b2a4999c
扶风 <fufeng@staruniongame.com>
2024-03-28 15:58:48 +0800

    Feat: rocketmq openmessaging message

---
+++ src/main/java/org/fufeng/knowledge/rocketmq/openmessaging/SimplePullConsumer.java
@@ -0,0 +1,67 @@
+package org.fufeng.knowledge.rocketmq.openmessaging;
+
+import io.openmessaging.Message;
+import io.openmessaging.MessagingAccessPoint;
+import io.openmessaging.OMS;
+import io.openmessaging.OMSBuiltinKeys;
+import io.openmessaging.consumer.PullConsumer;
+import io.openmessaging.producer.Producer;
+import io.openmessaging.producer.SendResult;
+import java.nio.charset.StandardCharsets;
+
+public class SimplePullConsumer {
+
+    public static final String URL = "oms:rocketmq://localhost:9876/default:default";
+    public static final String QUEUE = "OMS_CONSUMER";
+
+    public static void main(String[] args) {
+        // You need to set the environment variable OMS_RMQ_DIRECT_NAME_SRV=true
+
+        final MessagingAccessPoint messagingAccessPoint =
+                OMS.getMessagingAccessPoint(URL);
+
+        messagingAccessPoint.startup();
+
+        final Producer producer = messagingAccessPoint.createProducer();
+
+        final PullConsumer consumer = messagingAccessPoint.createPullConsumer(
+                OMS.newKeyValue().put(OMSBuiltinKeys.CONSUMER_ID, QUEUE));
+
+        messagingAccessPoint.startup();
+        System.out.printf("MessagingAccessPoint startup OK%n");
+
+        final String queueName = "TopicTest";
+
+        producer.startup();
+        Message msg = producer.createBytesMessage(queueName, "Hello Open Messaging".getBytes(StandardCharsets.UTF_8));
+        SendResult sendResult = producer.send(msg);
+        System.out.printf("Send Message OK. MsgId: %s%n", sendResult.messageId());
+        producer.shutdown();
+
+        consumer.attachQueue(queueName);
+
+        consumer.startup();
+        System.out.printf("Consumer startup OK%n");
+
+        // Keep running until we find the one that has just been sent
+        boolean stop = false;
+        while (!stop) {
+            Message message = consumer.receive();
+            if (message != null) {
+                String msgId = message.sysHeaders().getString(Message.BuiltinKeys.MESSAGE_ID);
+                System.out.printf("Received one message: %s%n", msgId);
+                consumer.ack(msgId);
+
+                if (!stop) {
+                    stop = msgId.equalsIgnoreCase(sendResult.messageId());
+                }
+
+            } else {
+                System.out.printf("Return without any message%n");
+            }
+        }
+
+        consumer.shutdown();
+        messagingAccessPoint.shutdown();
+    }
+}
---
+++ src/main/java/org/fufeng/knowledge/rocketmq/openmessaging/SimplePushConsumer.java
@@ -0,0 +1,39 @@
+package org.fufeng.knowledge.rocketmq.openmessaging;
+
+import io.openmessaging.Message;
+import io.openmessaging.MessagingAccessPoint;
+import io.openmessaging.OMS;
+import io.openmessaging.OMSBuiltinKeys;
+import io.openmessaging.consumer.PushConsumer;
+
+public class SimplePushConsumer {
+
+    public static final String URL = "oms:rocketmq://localhost:9876/default:default";
+    public static final String QUEUE = "OMS_HELLO_TOPIC";
+
+    public static void main(String[] args) {
+        // You need to set the environment variable OMS_RMQ_DIRECT_NAME_SRV=true
+
+        final MessagingAccessPoint messagingAccessPoint = OMS
+                .getMessagingAccessPoint(URL);
+
+        final PushConsumer consumer = messagingAccessPoint.
+                createPushConsumer(OMS.newKeyValue().put(OMSBuiltinKeys.CONSUMER_ID, "OMS_CONSUMER"));
+
+        messagingAccessPoint.startup();
+        System.out.printf("MessagingAccessPoint startup OK%n");
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            consumer.shutdown();
+            messagingAccessPoint.shutdown();
+        }));
+
+        consumer.attachQueue(QUEUE, (message, context) -> {
+            System.out.printf("Received one message: %s%n", message.sysHeaders().getString(Message.BuiltinKeys.MESSAGE_ID));
+            context.ack();
+        });
+
+        consumer.startup();
+        System.out.printf("Consumer startup OK%n");
+    }
+}
