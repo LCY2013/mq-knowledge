e18630ea79f9491c181f2634f76ced0f46d00577
扶风 <fufeng@staruniongame.com>
2024-03-28 16:08:34 +0800

    Feat: rocketmq order message

---
+++ src/main/java/org/fufeng/knowledge/rocketmq/ordermessage/Producer.java
@@ -0,0 +1,40 @@
+package org.fufeng.knowledge.rocketmq.ordermessage;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
+
+public class Producer {
+    public static void main(String[] args) throws MQClientException {
+        try {
+            DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+            producer.start();
+
+            String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
+            for (int i = 0; i < 100; i++) {
+                int orderId = i % 10;
+                Message msg =
+                        new Message("TopicTestjjj", tags[i % tags.length], "KEY" + i,
+                                ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                SendResult sendResult = producer.send(msg, (mqs, msg1, arg) -> {
+                    Integer id = (Integer) arg;
+                    int index = id % mqs.size();
+                    return mqs.get(index);
+                }, orderId);
+
+                System.out.printf("%s%n", sendResult);
+            }
+
+            producer.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new MQClientException(e.getMessage(), null);
+        }
+    }
+}
